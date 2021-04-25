e6e013aae5a6c1f5a0b7a285e0dec7a44e837025
osgun4christ <osgun4christ@gmail.com>
2021-04-25 01:12:37 +0000

    1st commit

---
+++ app/src/main/java/com/example/zuriincrement/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.zuriincrement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var plus = findViewById<Button>(R.id.button_increment)
+        var text = findViewById<TextView>(R.id.increment_textview)
+        var increment_number = 0;
+
+        plus.setOnClickListener{
+
+            increment_number++
+
+            text.text = increment_number.toString()
+        }
+
+    }
+}
